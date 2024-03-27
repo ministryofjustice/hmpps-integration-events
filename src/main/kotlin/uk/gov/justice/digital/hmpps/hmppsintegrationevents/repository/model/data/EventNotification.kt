e66ec530fbec618f8a8ec58ad235fd1d875110d2
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-03-27 15:43:35 +0000

    [HIA-685]
    WIP Adding more annotations to JPA entity and quick rename of DB schema to bring it in line with other names

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/repository/model/data/EventNotification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/repository/model/data/EventNotification.kt
@@ -2,26 +2,37 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.model.dat
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jakarta.persistence.Temporal
+import jakarta.persistence.TemporalType
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.model.enum.EventType
 import java.time.LocalDateTime
 
 @Entity
+@Table(name = "EVENT_NOTIFICATION", schema = "EVENT_STORE")
 class EventNotification(
 
   @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
   @Column(name = "EVENT_ID", nullable = false, unique = true)
   val eventId: Long,
 
   @Column(name = "HMPPS_ID", nullable = false)
   val hmppsId: String,
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "EVENT_TYPE", nullable = false)
   val eventType: EventType,
 
   @Column(name = "URL", nullable = false)
   val url: String,
 
+  @Temporal(value = TemporalType.TIMESTAMP)
   @Column(name = "LAST_MODIFIED_DATETIME", nullable = false)
   val lastModifiedDateTime: LocalDateTime,
 
