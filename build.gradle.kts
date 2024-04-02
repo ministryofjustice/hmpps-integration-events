63eebcb9ee2e2d2fa3666e9df1abf6bf4f1054a5
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-04-02 17:03:52 +0100

    Merge latest HIA685

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
-  kotlin("plugin.jpa") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  kotlin("plugin.spring") version "1.9.23"
+  kotlin("plugin.jpa") version "1.9.23"
   kotlin("plugin.lombok") version "1.9.23"
 }
 
@@ -19,13 +19,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-  testImplementation("org.testcontainers:localstack:1.19.7")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.1.0") {
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2") {
     exclude("org.springframework.security", "spring-security-config")
     exclude("org.springframework.security", "spring-security-core")
     exclude("org.springframework.security", "spring-security-crypto")
     exclude("org.springframework.security", "spring-security-web")
+    exclude("org.apache.common", "commons-compress")
   }
+  testImplementation("org.testcontainers:localstack:1.19.7")
+  testImplementation("org.apache.commons:commons-compress:1.26.1")
 }
 
 kotlin {
