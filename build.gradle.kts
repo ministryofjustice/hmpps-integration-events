93f62b8e4b04149e7f0aade7c3ef008d937ebcb3
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-04-03 11:34:29 +0100

    Hia 694 implement event notifier (#4)
    * Refactor to use JPA repository and Entity
    
    * Merge latest
    
    * lint
    
    * lint
    
    * Change to use flyway to auto generate/update database
    
    * Fix docker compose and setup test consumer queue
    
    * Remove event from database after publish to sns
    
    * Merge latest HIA685
    
    * update circuit ci docker compose version
    
    * Seperate docker-compose up and gradlew check
    
    * remove docker-compose up step
    
    * lint
    
    * Write message back to dead letter queue if failed to send event notification
    
    * update test config file and use h2 memory db for test
    
    ---------
    
    Co-authored-by: AlexHerbertMoJ <157111876+AlexHerbertMoJ@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
-  kotlin("plugin.jpa") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  kotlin("plugin.spring") version "1.9.23"
+  kotlin("plugin.jpa") version "1.9.23"
   kotlin("plugin.lombok") version "1.9.23"
 }
 
@@ -11,15 +11,27 @@ configurations {
 
 dependencies {
   runtimeOnly("org.postgresql:postgresql")
+  runtimeOnly("org.flywaydb:flyway-core")
 
   annotationProcessor("org.projectlombok:lombok:1.18.32")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2") {
+    exclude("org.springframework.security", "spring-security-config")
+    exclude("org.springframework.security", "spring-security-core")
+    exclude("org.springframework.security", "spring-security-crypto")
+    exclude("org.springframework.security", "spring-security-web")
+    exclude("org.apache.common", "commons-compress")
+  }
   testImplementation("org.testcontainers:localstack:1.19.7")
+  testImplementation("org.apache.commons:commons-compress:1.26.1")
+  testImplementation("io.kotest:kotest-assertions-json-jvm:5.8.0")
+  testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.0")
+  testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
+  testImplementation("com.h2database:h2:2.2.224")
 }
 
 kotlin {
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/EventNotifierService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/EventNotifierService.kt
@@ -1,18 +1,64 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationevents.services
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import software.amazon.awssdk.services.sns.SnsAsyncClient
+import software.amazon.awssdk.services.sns.model.PublishRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.EventNotificationRepository
+import uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.model.data.EventNotification
+import uk.gov.justice.hmpps.sqs.HmppsQueue
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
 import java.time.LocalDateTime
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue as snsMessageAttributeValue
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue as sqsMessageAttributeValue
 
 @Service
 class EventNotifierService(
+  private val hmppsQueueService: HmppsQueueService,
+  private val objectMapper: ObjectMapper,
   val eventRepository: EventNotificationRepository,
 ) {
+  private final val hmppsEventsTopicSnsClient: SnsAsyncClient
+  private final val topicArn: String
 
-  @Scheduled(fixedRate = 60000)
+  private val dlQueue by lazy { hmppsQueueService.findByQueueId("hmpps_integrations_events_queue_dlq") as HmppsQueue }
+  private val dlClient by lazy { dlQueue.sqsClient }
+  private val dlQueueUrl by lazy { dlQueue.queueUrl }
+  init {
+    val hmppsEventTopic = hmppsQueueService.findByTopicId("integrationeventtopic")
+    topicArn = hmppsEventTopic!!.arn
+    hmppsEventsTopicSnsClient = hmppsEventTopic.snsClient
+  }
+
+  @Scheduled(fixedRate = 10000)
   fun sentNotifications() {
     val fiveMinutesAgo = LocalDateTime.now().minusMinutes(5)
     val events = eventRepository.findAllWithLastModifiedDateTimeBefore(fiveMinutesAgo)
+    events.forEach { event -> sendEvent(event) }
+  }
+
+  fun sendEvent(payload: EventNotification) {
+    try {
+      hmppsEventsTopicSnsClient.publish(
+        PublishRequest.builder()
+          .topicArn(topicArn)
+          .message(objectMapper.writeValueAsString(payload))
+          .messageAttributes(mapOf("eventType" to snsMessageAttributeValue.builder().dataType("String").stringValue(payload.eventType.name).build())).build(),
+      )
+    } catch (e: Exception) {
+      dlClient.sendMessage(
+        SendMessageRequest.builder()
+          .queueUrl(dlQueueUrl)
+          .messageBody(
+            objectMapper.writeValueAsString(payload),
+          )
+          .messageAttributes(mapOf("Error" to sqsMessageAttributeValue.builder().dataType("String").stringValue(e.message).build()))
+          .build(),
+      )
+    }
+
+    eventRepository.deleteById(payload.eventId)
   }
 }
