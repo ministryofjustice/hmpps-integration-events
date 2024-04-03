993d64de5eb56b62235222274ff9137f9991470c
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-04-03 10:50:26 +0100

    [HIA-685]
    WIP Removing docker compose from circleCI, and small formatting changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/HmppsRegistrationListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/HmppsRegistrationListener.kt
@@ -6,12 +6,12 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
-
 @Service
 class HmppsRegistrationListener(private val objectMapper: ObjectMapper) {
   private companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
+
   @SqsListener("prisoner", factory = "hmppsQueueContainerFactoryProxy")
   fun onAuditEvent(rawMessage: String) {
     log.info("Received message {}", rawMessage)
