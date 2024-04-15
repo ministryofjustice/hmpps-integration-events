d814ac43173ec46d23fc879ad6a9ed373ace8979
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-04-15 17:16:10 +0100

    [HIA-687]
    Removing unnecessary enum values

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/models/enums/EventTypeValue.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/models/enums/EventTypeValue.kt
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.enums
 
-enum class EventTypeValue(val value: String, val url: String) {
-  ADDRESS_CHANGE("Address Change", ""),
-  REGISTRATION_ADDED("probation-case.registration.added", "/v1/persons/{hmppsId}/risks/mappadetail"),
+enum class EventTypeValue(val value: String) {
+  ADDRESS_CHANGE("Address Change"),
+  REGISTRATION_ADDED("probation-case.registration.added"),
   ;
 
   companion object {
