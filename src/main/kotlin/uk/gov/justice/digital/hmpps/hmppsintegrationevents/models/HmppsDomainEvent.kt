249d3bde4fc5e2adde055b6cfb2d99e2a1bd1957
Alex Pope <89132764+popey2700@users.noreply.github.com>
2024-04-15 10:47:30 +0100

    Implement deduplication event checks (#9)
    * Added initial DB check
    
    * Began handling different event types
    
    * Added handling into listener

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/models/HmppsDomainEvent.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/models/HmppsDomainEvent.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class HmppsDomainEvent(
   @JsonProperty("Type") val type: String,
-  @JsonProperty("Message") val message: DomainEventMessage,
+  @JsonProperty("Message") val message: String,
   @JsonProperty("MessageId") val messageId: String,
   @JsonProperty("MessageAttributes") val messageAttributes: DomainEventMessageAttributes,
 )
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/HmppsDomainEventsListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/HmppsDomainEventsListenerTest.kt
@@ -1,77 +1,43 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationevents.listeners
 
-import com.fasterxml.jackson.core.JsonParseException
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.function.Executable
 import org.mockito.Mockito
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.integration.helpers.SqsNotificationGeneratingHelper
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.AdditionalInformation
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.DomainEventMessage
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.DomainEventMessageAttributes
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.EventType
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.HmppsDomainEvent
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.Identifier
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.PersonReference
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.services.DomainEventsService
+import uk.gov.justice.digital.hmpps.hmppsintegrationevents.services.DeadLetterQueueService
+import uk.gov.justice.digital.hmpps.hmppsintegrationevents.services.RegistrationEventsService
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 class HmppsDomainEventsListenerTest {
 
-  val mockDomainEventsService: DomainEventsService = Mockito.mock(DomainEventsService::class.java)
-  val hmppsDomainEventsListener: HmppsDomainEventsListener = HmppsDomainEventsListener(domainEventsService = mockDomainEventsService)
+  val mockRegistrationEventsService: RegistrationEventsService = Mockito.mock(RegistrationEventsService::class.java)
+  val mockDlqService: DeadLetterQueueService = Mockito.mock(DeadLetterQueueService::class.java)
+  val hmppsDomainEventsListener: HmppsDomainEventsListener = HmppsDomainEventsListener(mockRegistrationEventsService, mockDlqService)
 
   @Test
-  fun `when a sqs event is received it should call the domainEventService`() {
+  fun `when a registration added sqs event is received it should call the registrationEventService`() {
     val currentTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
-    val rawMessage = SqsNotificationGeneratingHelper().generate(timestamp = currentTime)
+
+    val rawMessage = SqsNotificationGeneratingHelper(timestamp = currentTime).generateRegistrationEvent()
+
+    val isoInstantTimestamp = DateTimeFormatter.ISO_INSTANT.format(currentTime)
+    val readableTimestamp = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy").format(currentTime)
 
     val hmppsDomainEvent = HmppsDomainEvent(
       type = "Notification",
-      message = DomainEventMessage(
-        occurredAt = DateTimeFormatter.ISO_INSTANT.format(currentTime),
-        personReference = PersonReference(identifiers = listOf(Identifier(type = "CRN", value = "X777776"))),
-        additionalInformation = AdditionalInformation(registerTypeDescription = "MAPPA", registerTypeCode = "MAPP"),
-      ),
+      message = "{\"eventType\":\"probation-case.registration.added\",\"version\":1,\"occurredAt\":\"$isoInstantTimestamp\",\"description\":\"A new registration has been added to the probation case\",\"personReference\":{\"identifiers\":[{\"type\":\"CRN\",\"value\":\"X777776\"}]},\"additionalInformation\":{\"registrationLevelDescription\":\"MAPPA Level 3\",\"registerTypeDescription\":\"MAPPA\",\"registrationCategoryCode\":\"M1\",\"registrationId\":\"1234567890\",\"registrationDate\":\"$readableTimestamp\",\"registerTypeCode\":\"MAPP\",\"createdDateAndTime\":\"$readableTimestamp\",\"registrationCategoryDescription\":\"MAPPA Cat 1\",\"registrationLevelCode\":\"M3\"}}",
       messageId = "1a2345bc-de67-890f-1g01-11h21314h151",
       messageAttributes = DomainEventMessageAttributes(eventType = EventType(value = "probation-case.registration.added")),
     )
 
     hmppsDomainEventsListener.onDomainEvent(rawMessage)
 
-    verify(mockDomainEventsService, times(1)).execute(hmppsDomainEvent)
-  }
-
-  @Test
-  fun `Throws exception when the json object is not deserialisable`() {
-    val rawMessage = """
-      {
-       "Type" : ,
-       "MessageId" : "1a2345bc-de67-890f-1g01-11h21314h151",
-       "TopicArn" : "arn:aws:sns:eu-west-2:123456789123:cloud-platform-Digital-Prison-Services-12a3456bc12345a1a12345ab3c123b12",
-       "Message" : "This won't be able to deserialise into an object.",
-       "Timestamp" : "2023-03-25T10:35:38.403Z",
-       "SignatureVersion" : "1",
-       "Signature" : "A5cK8hNQj+3PTeLwS9D4bFYUvz1aI6cGvT2XWmRkE+7MoJZniFQjDeo2tI3BwKhLI9RXznGvZD1KbOoEMp4kUqBnA2Wr3bH5D6NfYhJKYV8sGpVcWzUMlTgbxErC1L7RgM2o7bHWlCKqfzOiDRsLpPeuJElmuTcYJtn+Lxv3R4TyYndafQSoVpErHcRJwWjDSf5l6jrKVa4m0NQbgtyZlxVPcHYf+wpWcuJ1BvLSnDHM+egRw7GqpxDB+IU1kEolC3eL4RUjXGmkQ9YtczF2P1JrThsRi0oEgnp7f/VtB6oqOKkYvlMXaF/+uhdwZbWlDa/83HcYV9MkJpWpx8UeisflEMNt57hbJXIgGQvsTYo4cRn9VgWQJxb6yDw8zS+I6yuGNbcL5L2Aj+whsEM1ovcR7KBPvgsMlxtCyVYzj36sDa4nUfiR5iZkVDeT==",
-       "SigningCertURL" : "https://sns.eu-west-2.amazonaws.com/SimpleNotificationService-12abcd123456d12b1e23a123456ef123.pem",
-       "UnsubscribeURL" : "https://sns.eu-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-2:123456789123:cloud-platform-Digital-Prison-Services-12a3456bc12345a1a12345ab3c123b12:123b456a-123f-1234-a123-123456789d10",
-       "MessageAttributes" : {
-         "eventType" : {"Type":"String","Value":"probation-case.registration.added"},
-         "id" : {"Type":"String","Value":"12345678-a1af-a0ba-1b22-d12e12d1234f"},
-         "timestamp" : {"Type":"Number.java.lang.Long","Value":"1711363538399"}
-       }
-     }
-    `"""
-
-    val executable = Executable { hmppsDomainEventsListener.onDomainEvent(rawMessage) }
-
-    assertThrows(
-      JsonParseException::class.java,
-      executable,
-    )
+    verify(mockRegistrationEventsService, times(1)).execute(hmppsDomainEvent)
   }
 }
