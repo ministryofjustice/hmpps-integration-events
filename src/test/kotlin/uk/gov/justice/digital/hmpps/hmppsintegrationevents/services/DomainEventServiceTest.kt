11a1675f30f5c046a62aadc109c914d970f0b7a8
AlexHerbertMadeTech <Alexandra.Herbert@madetech.com>
2024-04-15 11:07:58 +0100

    [HIA-687]
    WIP Comitting so I can merge more code in

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/DomainEventServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/DomainEventServiceTest.kt
@@ -4,6 +4,8 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
+import org.mockito.MockedStatic
+import org.mockito.Mockito.mockStatic
 import org.springframework.test.context.ActiveProfiles
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.AdditionalInformation
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.DomainEventMessage
@@ -19,13 +21,14 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
+
 @ActiveProfiles("test")
 class DomainEventServiceTest {
 
   private val repo = mockk<EventNotificationRepository>()
   val service: DomainEventsService = DomainEventsService(repo)
   val currentTime = LocalDateTime.now()
-  val currentTimeString =  currentTime.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_INSTANT)
+  val currentTimeString =  currentTime.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
   @Test
   fun `will process and save a mapps domain registration event message`() {
     val event = HmppsDomainEvent(
@@ -33,20 +36,23 @@ class DomainEventServiceTest {
       message = DomainEventMessage(
         occurredAt = currentTimeString,
         personReference = PersonReference(
-          identifiers = listOf((Identifier(type = "CRN", value = "X777776")))
+          identifiers = listOf((Identifier(type = "CRN", value = "X777776"))),
         ),
         additionalInformation = AdditionalInformation(
           registerTypeCode = "rTpeCode",
-          registerTypeDescription = "rTypeDesc"
-        )
+          registerTypeDescription = "rTypeDesc",
+        ),
       ),
       messageId = "abc123",
       messageAttributes = DomainEventMessageAttributes(
-      eventType = EventType(
-        value = EventTypeValue.REGISTRATION_ADDED.value
-      )
-      )
+        eventType = EventType(
+          value = EventTypeValue.REGISTRATION_ADDED.value,
+        ),
+      ),
     )
+
+
+    every { LocalDateTime.now() } returns currentTime
 
     every { repo.save(any()) } returnsArgument 0
 
@@ -56,7 +62,7 @@ class DomainEventServiceTest {
       eventType = EventTypeValue.REGISTRATION_ADDED,
       hmppsId = "X777776",
       url = "https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/X777776/risks/mappadetail",
-      lastModifiedDateTime = currentTime
+      lastModifiedDateTime = currentTime,
     )
 
     verify(exactly = 1) { repo.save(expectedEventSave) }
