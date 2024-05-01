412adea311ed318f047a0ba9353d75538d8115c6
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-05-01 15:10:07 +0100

    WIP: set variable as string

--- scripts/localstack/setup.sh
+++ scripts/localstack/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-AWS_REGION=eu-west-2
+AWS_REGION="eu-west-2"
 AWS_ACCESS_KEY_ID="test"
 AWS_SECRET_ACCESS_KEY="test"
 BACK_UP_BUCKET="certificate-backup"
 TEST_CLIENT_SECRET="testSecret"
 TEST_CLIENT_SECRET_VALUE="{\"eventType\":[\"default\"]}"
 ls
+echo $AWS_REGION
 aws configure set aws_default_region $AWS_REGION
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
