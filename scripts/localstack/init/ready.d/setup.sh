ff69222e31c2b23e82ab525e84acaf0754072dd5
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-05-01 14:38:57 +0100

    WIP: update the right file

--- scripts/localstack/init/ready.d/setup.sh
+++ scripts/localstack/init/ready.d/setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 AWS_REGION=eu-west-2
-AWS_ACCESS_KEY_ID="test"
-AWS_SECRET_ACCESS_KEY="test"
 BACK_UP_BUCKET="certificate-backup"
 TEST_CLIENT_SECRET="testSecret"
 TEST_CLIENT_SECRET_VALUE="{\"eventType\":[\"default\"]}"
-echo "Hello World"
 #Create certficate backup bucket
-aws --endpoint-url=http://localhost:4566 s3 mb s3://$BACK_UP_BUCKET
+awslocal s3 mb s3://$BACK_UP_BUCKET
 #Copy client certificate to bucket
-aws --endpoint-url=http://localhost:4566 s3 cp "/etc/localstack/client.p12" "s3://certificate-backup/testclient/client.p12"
-aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name $TEST_CLIENT_SECRET --description "Test Client Filter" --secret-string $TEST_CLIENT_SECRET_VALUE
+awslocal s3 cp "/etc/localstack/client.p12" "s3://certificate-backup/testclient/client.p12"
+awslocal secretsmanager create-secret --name $TEST_CLIENT_SECRET --description "Test Client Filter" --secret-string $TEST_CLIENT_SECRET_VALUE
 
