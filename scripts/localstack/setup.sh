e304361d82b2a6f1b2c1a841d8bc2aedebf12058
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-05-01 15:23:30 +0100

    WIP: set region

--- scripts/localstack/setup.sh
+++ scripts/localstack/setup.sh
@@ -7,12 +7,11 @@ TEST_CLIENT_SECRET="testSecret"
 TEST_CLIENT_SECRET_VALUE="{\"eventType\":[\"default\"]}"
 ls
 echo $AWS_REGION
-aws configure set aws_default_region $AWS_REGION
+aws configure set region $AWS_REGION
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 #Create certficate backup bucket
-aws --endpoint-url=http://localhost:4566 s3 mb s3://$BACK_UP_BUCKET --region $AWS_REGION
+aws --endpoint-url=http://localhost:4566 s3 mb s3://$BACK_UP_BUCKET
 #Copy client certificate to bucket
-aws --endpoint-url=http://localhost:4566 s3 cp "./scripts/localstack/client.p12" "s3://certificate-backup/testclient/client.p12" --region $AWS_REGION
-aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name $TEST_CLIENT_SECRET --description "Test Client Filter" --secret-string $TEST_CLIENT_SECRET_VALUE --region $AWS_REGION
-
+aws --endpoint-url=http://localhost:4566 s3 cp "./scripts/localstack/client.p12" "s3://certificate-backup/testclient/client.p12"
+aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name $TEST_CLIENT_SECRET --description "Test Client Filter" --secret-string $TEST_CLIENT_SECRET_VALUE
