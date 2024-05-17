1ca961798eaf6d9bd1ee6ea608c5bc1234ce1bbd
popey2700 <alex.pope@madetech.com>
2024-05-17 15:13:54 +0100

    Fixed local DB bug

--- scripts/localstack/setup.sh
+++ scripts/localstack/setup.sh
@@ -9,7 +9,7 @@ TEST_CLIENT_SECRET_VALUE="{\"eventType\":[\"default\"]}"
 aws configure set region $AWS_REGION
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-#Create certficate backup bucket
+#Create certificate backup bucket
 aws --endpoint-url=http://localhost:4566 s3 mb s3://$BACK_UP_BUCKET
 #Copy client certificate to bucket
 aws --endpoint-url=http://localhost:4566 s3 cp "./scripts/localstack/client.p12" "s3://certificate-backup/testclient/client.p12"
