c0ea1709ef077ebaf2dcfbd645d4ff4fbcbe909f
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-05-31 09:40:32 +0100

    Merge latest helm branch

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/IntegrationEventTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/IntegrationEventTest.kt
@@ -20,7 +20,7 @@ import org.springframework.test.context.ActiveProfiles
 import software.amazon.awssdk.services.sqs.model.Message
 import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest
-import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.enums.EventTypeValue
+import uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.enums.OutgoingEventType
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.EventNotificationRepository
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.repository.model.data.EventNotification
 import uk.gov.justice.digital.hmpps.hmppsintegrationevents.services.IntegrationEventTopicService
@@ -91,7 +91,7 @@ class IntegrationEventTest {
     await.atMost(10, TimeUnit.SECONDS).untilAsserted {
       eventRepository.save(
         EventNotification(
-          eventType = EventTypeValue.REGISTRATION_ADDED,
+          eventType = OutgoingEventType.MAPPA_DETAIL_CHANGED,
           hmppsId = "MockId",
           url = "MockUrl",
           lastModifiedDateTime = LocalDateTime.now().minusMinutes(6),
@@ -105,7 +105,7 @@ class IntegrationEventTest {
           ThrowingConsumer { event: String? ->
             JsonAssertions.assertThatJson(event)
               .node("eventType")
-              .isEqualTo(EventTypeValue.REGISTRATION_ADDED.name)
+              .isEqualTo(OutgoingEventType.MAPPA_DETAIL_CHANGED.name)
           },
         )
     }
