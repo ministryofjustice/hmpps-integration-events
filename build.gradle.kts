e9903572b9334c9a810e1c8814c9d92887710dc9
Bingjie Liu <bingjie.liu@digital.justice.gov.uk>
2024-07-08 10:36:36 +0100

    Fix security pipeline (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
   kotlin("plugin.spring") version "1.9.23"
   kotlin("plugin.jpa") version "1.9.23"
   kotlin("plugin.lombok") version "1.9.23"
