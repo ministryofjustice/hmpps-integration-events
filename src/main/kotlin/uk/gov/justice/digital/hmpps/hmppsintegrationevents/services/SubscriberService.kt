2dc10646588718358e815a5faa54c680fa548314
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-08-14 18:00:36 +0100

    PI-2372 - add new notification event

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/SubscriberService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/SubscriberService.kt
@@ -37,6 +37,7 @@ class SubscriberService(
           url.takeIf { it.contains("/v1/persons/.*/risks/dynamic") }?.let { IntegrationEventTypes.DYNAMIC_RISKS_CHANGED.name },
           url.takeIf { it.contains("/v1/persons/[^/]*$") }?.let { IntegrationEventTypes.PERSON_STATUS_CHANGED.name },
           url.takeIf { it.contains("/v1/persons/.*/alerts/pnd") }?.let { IntegrationEventTypes.PND_ALERTS_CHANGED.name },
+          url.takeIf { it.contains("/v1/persons/.*/licences/conditions") }?.let { IntegrationEventTypes.LICENCE_CONDITION_CHANGES.name },
         )
       }
       .ifEmpty { listOf("DEFAULT") }
