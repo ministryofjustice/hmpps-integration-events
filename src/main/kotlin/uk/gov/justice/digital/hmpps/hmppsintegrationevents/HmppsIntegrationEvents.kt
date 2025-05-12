6804966edc68ed8f70ef4309892918acdf0b640d
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-12 14:44:44 +0100

    Add conditional annotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/HmppsIntegrationEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/HmppsIntegrationEvents.kt
@@ -2,10 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationevents
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@Profile("!test")
 class HmppsIntegrationEvents
 
 fun main(args: Array<String>) {
