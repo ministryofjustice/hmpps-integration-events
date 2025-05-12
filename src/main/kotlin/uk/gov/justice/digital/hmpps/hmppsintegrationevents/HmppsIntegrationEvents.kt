4cfb227c3b1e45abc1d60a37965472d20e0f2001
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-12 14:54:23 +0100

    Moved config to seperate file

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/HmppsIntegrationEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/HmppsIntegrationEvents.kt
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
-@Profile("!test")
 class HmppsIntegrationEvents
 
 fun main(args: Array<String>) {
