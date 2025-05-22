31c7eb3eb4d6afae1a0044b98c80877e8184ca3f
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-22 15:56:39 +0100

    Remove unused import

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/EventSubscriberTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/EventSubscriberTest.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito.atLeast
 import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockReset.BEFORE
