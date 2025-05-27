8d1948ec21d438ee3fefa0316f0789ee12a465e5
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-27 12:17:21 +0100

    Use map to simplify getting the list of events available for the filter policy

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/SubscriberService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/SubscriberService.kt
@@ -32,26 +32,7 @@ class SubscriberService(
     clientConfig: Map.Entry<String, ConfigAuthorisation>,
     subscriber: HmppsSecretManagerProperties.SecretConfig,
   ) {
-    val events = clientConfig.value.endpoints
-      .flatMap { url ->
-        listOfNotNull(
-          url.takeIf { it.contains("/v1/persons/.*/risks/mappadetail") }?.let { IntegrationEventType.MAPPA_DETAIL_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/risks/scores") }?.let { IntegrationEventType.RISK_SCORE_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/sentences/latest-key-dates-and-adjustments") }?.let { IntegrationEventType.KEY_DATES_AND_ADJUSTMENTS_PRISONER_RELEASE.name },
-          url.takeIf { it.contains("/v1/persons/.*/status-information") }?.let { IntegrationEventType.PROBATION_STATUS_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/risks/dynamic") }?.let { IntegrationEventType.DYNAMIC_RISKS_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/[^/]*$") }?.let { IntegrationEventType.PERSON_STATUS_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/alerts/pnd") }?.let { IntegrationEventType.PND_ALERTS_CHANGED.name },
-          url.takeIf { it.contains("/v1/pnd/persons/.*/alerts") }?.let { IntegrationEventType.PND_ALERTS_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/licences/conditions") }?.let { IntegrationEventType.LICENCE_CONDITION_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/risks/serious-harm") }?.let { IntegrationEventType.RISK_OF_SERIOUS_HARM_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/plp-induction-schedule") }?.let { IntegrationEventType.PLP_INDUCTION_SCHEDULE_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/plp-review-schedule") }?.let { IntegrationEventType.PLP_REVIEW_SCHEDULE_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/addresses") }?.let { IntegrationEventType.PERSON_ADDRESS_CHANGED.name },
-          url.takeIf { it.contains("/v1/persons/.*/person-responsible-officer") }?.let { IntegrationEventType.RESPONSIBLE_OFFICER_CHANGED.name },
-        )
-      }
-      .ifEmpty { listOf("DEFAULT") }
+    val events = clientConfig.value.endpoints.mapNotNull { endpointMap[it]?.name }.ifEmpty { listOf("DEFAULT") }
     val prisonIds = clientConfig.value.filters?.prisons
 
     val secretValue = secretsManagerService.getSecretValue(subscriber.secretId)
@@ -63,5 +44,24 @@ class SubscriberService(
       secretsManagerService.setSecretValue(subscriber.secretId, filterPolicy)
       integrationEventTopicService.updateSubscriptionAttributes(subscriber.queueId, "FilterPolicy", filterPolicy)
     }
+  }
+
+  private val endpointMap: Map<String, IntegrationEventType> by lazy {
+    mapOf(
+      "/v1/persons/.*/risks/mappadetail" to IntegrationEventType.MAPPA_DETAIL_CHANGED,
+      "/v1/persons/.*/risks/scores" to IntegrationEventType.RISK_SCORE_CHANGED,
+      "/v1/persons/.*/sentences/latest-key-dates-and-adjustments" to IntegrationEventType.KEY_DATES_AND_ADJUSTMENTS_PRISONER_RELEASE,
+      "/v1/persons/.*/status-information" to IntegrationEventType.PROBATION_STATUS_CHANGED,
+      "/v1/persons/.*/risks/dynamic" to IntegrationEventType.DYNAMIC_RISKS_CHANGED,
+      "/v1/persons/[^/]*$" to IntegrationEventType.PERSON_STATUS_CHANGED,
+      "/v1/persons/.*/alerts/pnd" to IntegrationEventType.PND_ALERTS_CHANGED,
+      "/v1/pnd/persons/.*/alerts" to IntegrationEventType.PND_ALERTS_CHANGED,
+      "/v1/persons/.*/licences/conditions" to IntegrationEventType.LICENCE_CONDITION_CHANGED,
+      "/v1/persons/.*/risks/serious-harm" to IntegrationEventType.RISK_OF_SERIOUS_HARM_CHANGED,
+      "/v1/persons/.*/plp-induction-schedule" to IntegrationEventType.PLP_INDUCTION_SCHEDULE_CHANGED,
+      "/v1/persons/.*/plp-review-schedule" to IntegrationEventType.PLP_REVIEW_SCHEDULE_CHANGED,
+      "/v1/persons/.*/addresses" to IntegrationEventType.PERSON_ADDRESS_CHANGED,
+      "/v1/persons/.*/person-responsible-officer" to IntegrationEventType.RESPONSIBLE_OFFICER_CHANGED,
+    )
   }
 }
