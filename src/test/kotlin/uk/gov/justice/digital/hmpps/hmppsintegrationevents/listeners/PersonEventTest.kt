f406147cd1d8e0bbd5f41a8d124d658f20ae7493
Tom Watkins <tom.watkins@justice.gov.uk>
2025-05-29 19:13:56 +0100

    Process new person events

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/PersonEventTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/listeners/PersonEventTest.kt
@@ -56,4 +56,44 @@ class PersonEventTest {
 
     verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_STATUS_CHANGED) }
   }
+
+  @ParameterizedTest
+  @ValueSource(
+    strings = [
+      HmppsDomainEventName.ProbabtionCase.Engagement.CREATED,
+      HmppsDomainEventName.ProbabtionCase.PrisonIdentifier.ADDED,
+      HmppsDomainEventName.PrisonerOffenderSearch.Prisoner.CREATED,
+      HmppsDomainEventName.PrisonerOffenderSearch.Prisoner.RECEIVED,
+    ],
+  )
+  fun `process new person events`(eventType: String) {
+    val message = when (eventType) {
+      HmppsDomainEventName.ProbabtionCase.Engagement.CREATED -> PROBATION_CASE_ENGAGEMENT_CREATED_MESSAGE
+      HmppsDomainEventName.ProbabtionCase.PrisonIdentifier.ADDED -> PROBATION_CASE_PRISON_IDENTIFIER_ADDED
+      HmppsDomainEventName.PrisonerOffenderSearch.Prisoner.CREATED -> PRISONER_OFFENDER_SEARCH_PRISONER_CREATED
+      HmppsDomainEventName.PrisonerOffenderSearch.Prisoner.RECEIVED -> PRISONER_OFFENDER_SEARCH_PRISONER_RECEIVED
+      else -> throw RuntimeException("Unexpected event type: $eventType")
+    }
+
+    val hmppsMessage = message.replace("\\", "")
+    val payload = generateDomainEvent(eventType, message)
+    val hmppsDomainEvent = generateHmppsDomainEvent(eventType, hmppsMessage)
+
+    every { hmppsDomainEventService.execute(hmppsDomainEvent, any()) } just runs
+
+    hmppsDomainEventsListener.onDomainEvent(payload)
+
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_STATUS_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_CASE_NOTES_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_NAME_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_SENTENCES_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_PROTECTED_CHARACTERISTICS_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_REPORTED_ADJUDICATIONS_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_NUMBER_OF_CHILDREN_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_PHYSICAL_CHARACTERISTICS_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_IMAGES_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_HEALTH_AND_DIET_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_CARE_NEEDS_CHANGED) }
+    verify(exactly = 1) { hmppsDomainEventService.execute(hmppsDomainEvent, IntegrationEventType.PERSON_LANGUAGES_CHANGED) }
+  }
 }
