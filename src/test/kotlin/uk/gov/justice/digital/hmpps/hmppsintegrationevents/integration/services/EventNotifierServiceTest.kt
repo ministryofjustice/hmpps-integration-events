92f42e4d9abaeee140d2df80f9223cfa6b1984f1
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-08-22 10:47:50 +0100

    HIA-838: Applied revire comments

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/EventNotifierServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/integration/services/EventNotifierServiceTest.kt
@@ -8,10 +8,8 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.AdditionalAnswers
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doNothing
-import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
