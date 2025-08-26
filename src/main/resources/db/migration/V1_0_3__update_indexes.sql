a7e78c7a0b973c23983d1dd93c77ab59b9d8baf8
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-08-26 13:48:40 +0100

    HIA-838: Add tests and removed JPA ddl create in favour of flyway

--- src/main/resources/db/migration/V1_0_3__update_indexes.sql
+++ src/main/resources/db/migration/V1_0_3__update_indexes.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX idx_event_notification_url_event_type_status ON EVENT_NOTIFICATION(URL, EVENT_TYPE, STATUS);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_event_notification_url_event_type_status ON EVENT_NOTIFICATION(URL, EVENT_TYPE, STATUS);
 
 DROP INDEX IF EXISTS idx_event_notification_url_event_type;
