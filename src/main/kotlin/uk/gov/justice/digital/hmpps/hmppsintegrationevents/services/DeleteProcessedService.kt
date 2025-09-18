8dd1075c2d66f63527e68557c99c62ecea718ad1
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-09-18 11:17:17 +0100

    HIA-930: Added logging to scheduled tasks

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/DeleteProcessedService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/DeleteProcessedService.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsintegrationevents.services
 
 import io.sentry.Sentry
 import jakarta.transaction.Transactional
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -14,14 +15,21 @@ class DeleteProcessedService(
   val eventRepository: EventNotificationRepository,
 ) {
 
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   @Scheduled(fixedRateString = "\${notifier.schedule.rate}")
   @Transactional
   fun deleteProcessedEvents() {
     val cutOff = LocalDateTime.now().minusHours(24)
     try {
+      log.info("Deleting processed events older than $cutOff")
       eventRepository.deleteEvents(cutOff)
     } catch (e: Exception) {
+      log.error("Error deleting processed events", e)
       Sentry.captureException(e)
     }
+    log.info("Successfully deleted processed events")
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/StateEventNotifierService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/StateEventNotifierService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsintegrationevents.services
 
 import io.sentry.Sentry
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -14,22 +15,35 @@ class StateEventNotifierService(
   private val integrationEventTopicService: IntegrationEventTopicService,
   val eventRepository: EventNotificationRepository,
 ) {
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
   @Scheduled(fixedRateString = "\${notifier.schedule.rate}")
   fun sentNotifications() {
     val fiveMinutesAgo = LocalDateTime.now().minusMinutes(5)
 
     val claimId = UUID.randomUUID().toString()
+
+    log.info("Setting to processing with claim id $claimId")
     // Claim records to process
     eventRepository.setProcessing(fiveMinutesAgo, claimId)
 
+    log.info("Set processing with claim id $claimId")
+
     val events = eventRepository.findAllProcessingEvents(claimId)
+
+    log.info("Sending ${events.size} events for claim id $claimId")
     events.forEach {
       try {
         integrationEventTopicService.sendEvent(it)
         eventRepository.setProcessed(it.eventId!!)
       } catch (e: Exception) {
+        log.error("Error caught with msg ${e.message} for claim id $claimId", e)
         Sentry.captureException(e)
       }
     }
+    log.info("Successfully sent ${events.size} events for claim id $claimId")
   }
 }
