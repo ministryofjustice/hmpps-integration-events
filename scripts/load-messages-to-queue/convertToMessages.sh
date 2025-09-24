7bde5736a2121c9b41869e131da03f824cef4ed6
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-09-24 11:30:31 +0100

    Hia 907 productionise pnd process (#186)
    * HIA-907: Created script to push messages to a queue
    
    * HIA-907: Updated scripts and fixed flakey test

---
+++ scripts/load-messages-to-queue/convertToMessages.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+##
+## Converts the results of an SQL query containing tab delimited file (containing event-id, crn and the last updated datetime)
+## into a domain event capable of being loaded into an hmpps-integration-api event queue
+## e.g
+##
+## 1502011403      A123456 2017-03-15T15:47:51.000000
+## 1502011452      A123457 2025-03-15T13:27:03.000000
+##
+## Usage:
+## /convertToMessages.sh -t <EVENT_TYPE> -e <ENV> -f <INPUT>
+## e.g ./convertToMessages.sh -e dev -t PERSON_STATUS_CHANGED -f sqlResults.txt > eventMessages.txt
+## returns
+##
+## {"Type":"Notification","MessageId":"...","TopicArn":"...","Message":"{\"eventId\":166,\"hmppsId\":\"A123456\",\"eventType\":\"PERSON_STATUS_CHANGED\",\"prisonId\":\"TRN\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A123456\",\"lastModifiedDateTime\":\"2017-03-15T15:47:51.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"prisonId":{"Type":"String","Value":"TRN"},"eventType":{"Type":"String","Value":"PERSON_STATUS_CHANGED"}}}
+## {"Type":"Notification","MessageId":"...","TopicArn":"...","Message":"{\"eventId\":166,\"hmppsId\":\"A123457\",\"eventType\":\"PERSON_STATUS_CHANGED\",\"prisonId\":\"TRN\",\"url\":\"https://dev.integration-api.hmpps.service.justice.gov.uk/v1/persons/A123457\",\"lastModifiedDateTime\":\"2025-03-15T13:27:03.000000\"}","Timestamp":"2025-09-24T09:39:24.000Z","SignatureVersion":"1","Signature":"...","SigningCertURL":"...","UnsubscribeURL":"...","MessageAttributes":{"prisonId":{"Type":"String","Value":"TRN"},"eventType":{"Type":"String","Value":"PERSON_STATUS_CHANGED"}}}
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -t eventType -e environment -f fileName"
+   echo -e "\t-t Provide an eventType"
+   echo -e "\t-e Provide an environment"
+   echo -e "\t-f Provide a file name containing the file you want to convert"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "t:e:f:" opt
+do
+   case "$opt" in
+      t ) EVENT_TYPE="$OPTARG" ;;
+      e ) ENV="$OPTARG" ;;
+      f ) INPUT="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+
+if [ -z "$ENV" ]
+then
+    echo "environment not specified, please specify an environment: (dev/preprod/prod)";
+    helpFunction
+fi
+
+if ! [[ "$ENV" =~ ^(dev|preprod|prod)$ ]]
+then
+    echo "Environment is not valid (dev/preprod/prod)";
+    helpFunction
+fi
+
+if [ -z "$EVENT_TYPE" ]
+then
+    echo "Event type not specified, please specify an event type: e.g PERSON_STATUS_CHANGED";
+    helpFunction
+fi
+
+if [ -z "$INPUT" ]
+then
+    echo "No file to convert has been specified";
+    helpFunction
+fi
+
+if ! [ -f "$INPUT" ]
+then
+    echo "File $INPUT cannot be found";
+    helpFunction
+fi
+
+while IFS= read -r line
+do
+  ARR=($line)
+  EVENT_ID=166 #Arbitary - or could be ${ARR[0]}
+  CRN=${ARR[1]}
+  LAST_UPDATED=${ARR[2]}
+  TIMESTAMP=$(date -u '+%Y-%m-%dT%H:%M:%S.000Z')
+  date=$(echo $(($(date +'%s * 1000 + %-N / 1000000'))))
+  URL="https://$ENV.integration-api.hmpps.service.justice.gov.uk/v1/persons/$CRN"
+  echo "{\"Type\":\"Notification\",\"MessageId\":\"...\",\"TopicArn\":\"...\",\"Message\":\"{\\\"eventId\\\":$EVENT_ID,\\\"hmppsId\\\":\\\"$CRN\\\",\\\"eventType\\\":\\\"$EVENT_TYPE\\\",\\\"prisonId\\\":\\\"TRN\\\",\\\"url\\\":\\\"$URL\\\",\\\"lastModifiedDateTime\\\":\\\"$LAST_UPDATED\\\"}\",\"Timestamp\":\"$TIMESTAMP\",\"SignatureVersion\":\"1\",\"Signature\":\"...\",\"SigningCertURL\":\"...\",\"UnsubscribeURL\":\"...\",\"MessageAttributes\":{\"prisonId\":{\"Type\":\"String\",\"Value\":\"TRN\"},\"eventType\":{\"Type\":\"String\",\"Value\":\"$EVENT_TYPE\"}}}"
+
+done < "$INPUT"
