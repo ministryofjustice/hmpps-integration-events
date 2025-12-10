646ddcd669cd6714dab5c7ed219c2392e92169dd
Rick Choi <rick.choi@justice.gov.uk>
2025-12-10 15:00:51 +0000

    ESWE-1559 Fix constraint issue for Integration Event Type changes (#224)
    - Drop constraint `event_notification_event_type_check`
    - Add a test to detect event type changes (hash code)

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/models/enums/IntegrationEventTypeTest.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.hmppsintegrationevents.models.enums
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class IntegrationEventTypeTest {
+  /**
+   * This test will fail when there is unexpected enum value(s)
+   *
+   * The enum [IntegrationEventType] is persisted to DB, and please consider these while making changes
+   * - Adding new value is safe
+   * - Renaming current value is unsafe
+   * - Removing current value is unsafe
+   *
+   * For renaming or removal of current value, please provide transition period
+   * i) Mark deprecated
+   * ii) Wait for transition period over
+   * iii) Finally remove it
+   */
+  @Test
+  fun `should have all event types expected`() {
+    val expectedEventTypesHashCode = 803935411
+
+    val actualEventTypesHashCode = IntegrationEventType.entries.map { it.name }.sorted().joinToString(",") { "\"$it\"" }.hashCode()
+
+    assertEquals(expectedEventTypesHashCode, actualEventTypesHashCode)
+  }
+}
