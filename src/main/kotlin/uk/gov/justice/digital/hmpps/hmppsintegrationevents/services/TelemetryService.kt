d8498e4d253a397bdf75c52e520cbb4044fac2ac
rickchoijd <rick.choi@justice.gov.uk>
2026-01-06 15:52:42 +0000

    ESWE-1516 Add documentation of `TelemetryService`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/TelemetryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsintegrationevents/services/TelemetryService.kt
@@ -4,6 +4,11 @@ import io.sentry.Sentry
 import org.jetbrains.annotations.NotNull
 import org.springframework.stereotype.Service
 
+/**
+ * This is a service facade of Telemetry.
+ *
+ * It shall cover tools like Sentry ([io.sentry.Sentry]), Application Insights (e.g. [com.microsoft.applicationinsights.TelemetryClient]), etc.
+ */
 @Service
 class TelemetryService {
 
